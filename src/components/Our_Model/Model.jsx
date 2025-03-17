95b62de80a7eea3fe05edbcb9beb5226f264a001
Shauviq <shauviqmishra@gmail.com>
2025-03-17 22:33:51 +0530

    Our Model Page done

--- src/components/Our_Model/Model.jsx
+++ src/components/Our_Model/Model.jsx
@@ -1,10 +1,16 @@
+import HeroSection from "../Hero Section/Hero_Sec";
 import Navbar from "../navbar/Navbar";
+import Footer from "../Footer/Footer";
+import ModelInfo from "../model_info/ModelInfo";
 import "./Model.css";
 
 function Model(){
     return(
         <main>
             <Navbar/>
+            <HeroSection/>
+            <ModelInfo/>
+            <Footer/>
         </main>
     );
 }
